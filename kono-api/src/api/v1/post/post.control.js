c4f73d36b4e78c1474e3e4adbf755904c18d4f31
inhibitor1217 <cytops1217@gmail.com>
2019-09-30 13:50:44 +0900

    implement image retrieving query

--- kono-api/src/api/v1/post/post.control.js
+++ kono-api/src/api/v1/post/post.control.js
@@ -1,15 +1,16 @@
-import { retrieve, count } from '../../../db/models/post';
+import { retrieve as postRetrieve, count } from '../../../db/models/post';
+import { retrieve as imageRetrieve } from '../../../db/models/image';
 
 export const list = async (req, res) => {
 
     const {
-        filter, 
+        filter_type, 
         start_index: startIndex, 
         max_size: maxSize 
     } = req.query;
     
     /* Query validity check. */
-    if (filter !== undefined && filter !== 'notice' && filter !== 'lostfound') {
+    if (filter_type !== undefined && filter_type !== 'notice' && filter_type !== 'lostfound') {
         res.status(400);
         res.send({ msg: 'invalid filter' });
         return;
@@ -40,14 +41,20 @@ export const list = async (req, res) => {
     }
 
     /* Fire database query. */
+    const filter = {
+        type: filter_type,
+        deleted: false
+    };
+    Object.keys(filter).forEach(key => filter[key] === undefined && delete filter[key]);
+
     try {
 
         const size = await count({
-            filter: { type: filter }
+            filter
         });
 
-        const posts = await retrieve({
-            filter: { type: filter },
+        const posts = await postRetrieve({
+            filter,
             select: [ 
                 'sid', 
                 'type', 
@@ -70,6 +77,63 @@ export const list = async (req, res) => {
 
 };
 
-export const single = (req, res) => {
-    res.end('/api/v1/post/:sid');
+export const single = async (req, res) => {
+
+    const { sid } = req.params;
+
+    /* Query validity check. */
+    if (isNaN(parseInt(sid)) || sid <= 0) {
+        res.status(400);
+        res.send({ msg: 'invalid sid' });
+        return;
+    }
+
+    /* Fire database query. */
+    const filter = { sid: parseInt(sid) };
+    
+    try {
+
+        const [post] = await postRetrieve({
+            filter,
+            select: [
+                'sid',
+                'type',
+                'title_kr',
+                'title_en',
+                'created_time',
+                'content_kr',
+                'content_en',
+                'deleted'
+            ],
+            maxSize: 1
+        });
+
+        if (!post || post.deleted === 1) {
+            res.status(404);
+            res.send({ msg: 'post does not exist' });
+            return;
+        }
+
+        const { deleted, ...data } = post;
+
+        const contentImages = await imageRetrieve({
+            filter: { post_sid: post.sid },
+            select: [
+                'url'
+            ],
+            maxSize: 64
+        }).then(rows => rows.map(row => row.url));
+
+        res.status(200);
+        res.send({ 
+            ...data, 
+            'content_img': contentImages
+        });
+
+    } catch (e) {
+        console.log(e);
+        res.status(500);
+        res.send({ msg: 'server error' });
+    }
+
 };
--- kono-api/src/db/models/post.js
+++ kono-api/src/db/models/post.js
@@ -11,19 +11,59 @@ const columns = [
     'deleted'
 ];
 
+function checkValidity(column, value) {
+    switch (column) {
+        case 'sid':
+            if (typeof(value) !== 'number')
+                throw new Error(`invalid type for sid: ${typeof(value)}`);
+            if (value <= 0)
+                throw new Error(`invalid value for sid: ${value}`);
+            break;
+        case 'type':
+            if (typeof(value) !== 'string')
+                throw new Error(`invalid type for type: ${typeof(value)}`);
+            if (value !== 'notice' && value !== 'lostfound')
+                throw new Error(`invalid value for type: ${value}`);
+            break;
+        case 'title_kr':
+            break;
+        case 'title_en':
+            break;
+        case 'created_time':
+            break;
+        case 'content_kr':
+            break;
+        case 'content_en':
+            break;
+        case 'deleted':
+            if (typeof(value) !== 'boolean')
+                throw new Error(`invalid type for deleted: ${typeof(value)}`);
+            break;
+        default:
+            throw new Error(`invalid column name: ${column}`);
+    }
+    return true;
+}
+
+function getFilterQueryString(filter) {
+    return Object.keys(filter)
+        .map(key => `${key} = ${typeof(filter[key]) === 'string' ? `\"${filter[key]}\"` : filter[key]}`)
+        .join(' AND ');
+}
+
 export const count = (countQuery) => {
 
     const { filter } = countQuery || {};
-    const { type } = filter || {};
 
-    if (type) {
-        if (type !== 'notice' && type !== 'lostfound')
-            throw new Error(`invalid filter type: ${type}`);
-    }
+    /* Argument validity check. */
+    if (Object.keys(filter).length == 0)
+        throw new Error('should provide at least one filter');
+    Object.keys(filter).every(key => checkValidity(key, filter[key]));
 
     const fn = 'SELECT COUNT(*) FROM post'
-        + ' WHERE deleted = 0'
-        + (type ? ` AND type = "${type}"` : '');
+        + ' WHERE ' + getFilterQueryString(filter);
+
+    console.log('post.count: ' + fn);
 
     return db.query(fn).then(result => result[0]['COUNT(*)']);
 
@@ -32,19 +72,18 @@ export const count = (countQuery) => {
 export const retrieve = (retrieveQuery) => {
 
     const { filter, select, startIndex, maxSize } = retrieveQuery || {};
-    const { type } = filter || {};
 
+    /* Argument validity check. */
     if (!maxSize)
         throw new Error('maxSize field required in retrieveQuery');
-    if (type) {
-        if (type !== 'notice' && type !== 'lostfound')
-            throw new Error(`invalid filter type: ${type}`);
-    }
+    if (Object.keys(filter).length == 0)
+        throw new Error('should provide at least one filter');
+    Object.keys(filter).every(key => checkValidity(key, filter[key]));
     if (select) {
         if (typeof(select) !== 'object')
             throw new Error(`invalid type for select: ${typeof(select)}`);
         if (!select.every(el => columns.some(col => el === col)))
-            throw new Error(`invalid column name for select: ${select}}`);
+            throw new Error(`invalid value for select: ${select}`);
     }
     if (startIndex) {
         if (typeof(startIndex) !== 'number')
@@ -58,10 +97,11 @@ export const retrieve = (retrieveQuery) => {
         throw new Error(`invalid value for maxSize: ${maxSize}`);
 
     const fn = `SELECT ${select === undefined ? '*' : `${select.join(', ')}`} FROM post`
-        + ' WHERE deleted = 0'
-        + (type ? ` AND type = "${type}"` : '')
+        + ' WHERE ' + getFilterQueryString(filter)
         + ` ORDER BY created_time DESC`
         + ` LIMIT ${(startIndex ? startIndex : 0)}, ${maxSize}`;
+
+    console.log('post.retrieve: ' + fn);
 
     return db.query(fn);
 
