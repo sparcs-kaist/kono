d998933aa5030be82c099c74989a956f6944bfa8
inhibitor1217 <cytops1217@gmail.com>
2019-10-01 10:34:59 +0900

    minor change with syntactic sugar

--- kono-front/src/store/modules/config.js
+++ kono-front/src/store/modules/config.js
@@ -97,10 +97,7 @@ export const ConfigMiddleWare = store => next => action => {
         case SET_TO_LOCAL_STORAGE_LANGUAGE:
             action.language = storage.get(STORAGE_KEY_LANGUAGE); // Attempt to retrieve from storage
             if (!action.language) {
-                action.language = userLanguage; // Attempt to retrieve from browser configuration
-                if (!action.language) {
-                    action.language = LANGUAGE_KR;
-                }
+                action.language = userLanguage || LANGUAGE_KR;   // Attempt to retrieve from browser configuration
                 storage.set(STORAGE_KEY_LANGUAGE, action.language);
             }
             break;
