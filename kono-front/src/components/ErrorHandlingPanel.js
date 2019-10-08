8c3989646a6dea032d74d6f7785a6bd6d7047482
inhibitor1217 <cytops1217@gmail.com>
2019-10-08 13:49:58 +0900

    add ErrorHandlingPanel

---
+++ kono-front/src/components/ErrorHandlingPanel.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import style from '../styles/ErrorHandlingPanel.module.scss';
+import MaterialIcon from './MaterialIcon';
+import classnames from '../lib/classnames';
+import useLanguages from '../lib/hooks/useLanguages';
+import Text from '../res/texts/ErrorHandlingPanel.text.json';
+
+export const ERROR_NONE = 'none';
+export const ERROR_CONN = 'connection_error';
+export const ERROR_400 = 'bad_request_error';
+export const ERROR_500 = 'internal_server_error';
+export const ERROR_DEFAULT = 'default_error';
+
+export default ({ isLoading, errorCode, height }) => {
+    
+    const text = useLanguages(Text);
+
+    const showError = !isLoading && (errorCode !== ERROR_NONE);
+
+    return (
+        <div 
+            className={
+                classnames([
+                    style.ErrorHandlingPanel,
+                    !isLoading && style.ErrorHandlingPanel__use_background
+                ])
+            }
+            style={{ height }}
+        >
+            {
+                isLoading && (
+                    <div>
+                        Spinner
+                    </div>
+                )
+            }
+            {
+                showError && (
+                    <>
+                        <MaterialIcon md48>error_outline</MaterialIcon>
+                        <div>
+                            { text[errorCode] }
+                        </div>
+                    </>
+                )
+            }
+        </div>
+    )
+
+}
--- kono-front/src/components/NoticePanel.js
+++ kono-front/src/components/NoticePanel.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import styles from '../styles/NoticePanel.module.scss';
 import PanelHeader from './PanelHeader';
 import PanelFooter from './PanelFooter';
+import ErrorHandlingPanel, * as errorCodes from './ErrorHandlingPanel';
 import * as PostAPI from '../api/post';
 import Text from '../res/texts/NoticePanel.text.json';
 import useLanguages from '../lib/hooks/useLanguages';
@@ -19,43 +20,77 @@ export default () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [numNotices, setNumNotices] = useState(0);
     const [notices, setNotices] = useState([]);
+    const [isLoadingCount, setLoadingCount] = useState(false);
+    const [isLoadingPage, setLoadingPage] = useState(false);
+    const [errorCode, setErrorCode] = useState(errorCodes.ERROR_NONE);
     const text = useLanguages(Text);
     const language = useSelector(state => state.config.language, []);
 
     const numPages = Math.max(1, Math.ceil(numNotices / NOTICE_PAGINATION));
 
-    const fetchCount = async () => {
-        await PostAPI.count()
-            .then(({ data }) => {
-                setNumNotices(data['notice']);
-            })
-            .catch(({ response }) => {
-                console.log(response);
-            })
-    }
-
-    const fetchPage = async (page) => {
-        await PostAPI.list({
-                params: {
-                    filter_type: 'notice',
-                    start_index: (page - 1) * NOTICE_PAGINATION,
-                    max_size: NOTICE_PAGINATION
-                }
-            })
-            .then(({ data }) => {
-                setNotices(data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
+    const isLoading = isLoadingCount || isLoadingPage;
+    const showNoticeList = !isLoading && (errorCode === errorCodes.ERROR_NONE);
 
     useEffect(() => {
+        setLoadingCount(true);
+        const fetchCount = async () => {
+            await PostAPI.count()
+                .then(({ data }) => {
+                    setLoadingCount(false);
+                    setNumNotices(data['notice']);
+                })
+                .catch(({ response }) => {
+                    if (!response)
+                        setErrorCode(errorCodes.ERROR_CONN);
+                    setLoadingCount(false);
+                    const { status } = response;
+                    switch (status) {
+                        case 400:
+                            setErrorCode(errorCodes.ERROR_400);
+                            break;
+                        case 500:
+                            setErrorCode(errorCodes.ERROR_500);
+                            break;
+                        default:
+                            setErrorCode(errorCodes.ERROR_DEFAULT);
+                    }
+                });
+        }
         fetchCount();
     }, []);
 
     /* Fetch page when currentPage updates */
     useEffect(() => {
+        const fetchPage = async (page) => {
+            setLoadingPage(true);
+            await PostAPI.list({
+                    params: {
+                        filter_type: '?',
+                        start_index: (page - 1) * NOTICE_PAGINATION,
+                        max_size: NOTICE_PAGINATION
+                    }
+                })
+                .then(({ data }) => {
+                    setLoadingPage(false);
+                    setNotices(data);
+                })
+                .catch(({ response }) => {
+                    if (!response)
+                        setErrorCode(errorCodes.ERROR_CONN);
+                    setLoadingPage(false);
+                    const { status } = response;
+                    switch (status) {
+                        case 400:
+                            setErrorCode(errorCodes.ERROR_400);
+                            break;
+                        case 500:
+                            setErrorCode(errorCodes.ERROR_500);
+                            break;
+                        default:
+                            setErrorCode(errorCodes.ERROR_DEFAULT);
+                    }
+                });
+        };
         fetchPage(currentPage);
     }, [currentPage]);
 
@@ -71,38 +106,51 @@ export default () => {
     return (
         <div className={styles.NoticePanel}>
             <PanelHeader title={text.title} link="/notice"/>
-            <ul>
-                {
-                    notices.map(({
-                        sid, title_kr, title_en, created_time
-                    }) => {
-                        
-                        const title = language === 'kr' ? title_kr : title_en; // cannot use useLangauge Hook: overlapping hooks
-                        const date = new Date(created_time);
+            {
+                !showNoticeList && (
+                    <ErrorHandlingPanel
+                        isLoading={isLoading}
+                        errorCode={errorCode}
+                        height={287}
+                    />
+                )
+            }
+            {
+                showNoticeList && (
+                    <ul>
+                        {
+                            notices.map(({
+                                sid, title_kr, title_en, created_time
+                            }) => {
+                                
+                                const title = language === 'kr' ? title_kr : title_en; // cannot use useLangauge Hook: overlapping hooks
+                                const date = new Date(created_time);
 
-                        return (
-                            <li key={`notice-${sid}`}>
-                                <div className={styles.NoticePanel__item}>
-                                    <span className={styles.NoticePanel__item_title}>
-                                        <Link to={`/post/${sid}`}>
-                                            { toTitleString(title) }
-                                        </Link>
-                                    </span>
-                                    <span className={styles.NoticePanel__item_date}>
-                                        { 
-                                            date.toLocaleString('default', {
-                                                year:  'numeric',
-                                                month: '2-digit',
-                                                day:   '2-digit'
-                                            })
-                                        }
-                                    </span>
-                                </div>
-                            </li>
-                        );
-                    })
-                }
-            </ul>
+                                return (
+                                    <li key={`notice-${sid}`}>
+                                        <div className={styles.NoticePanel__item}>
+                                            <span className={styles.NoticePanel__item_title}>
+                                                <Link to={`/post/${sid}`}>
+                                                    { toTitleString(title) }
+                                                </Link>
+                                            </span>
+                                            <span className={styles.NoticePanel__item_date}>
+                                                { 
+                                                    date.toLocaleString('default', {
+                                                        year:  'numeric',
+                                                        month: '2-digit',
+                                                        day:   '2-digit'
+                                                    })
+                                                }
+                                            </span>
+                                        </div>
+                                    </li>
+                                );
+                            })
+                        }
+                    </ul>
+                )
+            }
             <PanelFooter 
                 currentPage={currentPage} 
                 numPages={numPages}
