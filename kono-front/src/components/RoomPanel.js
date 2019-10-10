1ebe92f6fdbd2b3c44b6423dffd730fdd6a04a31
inhibitor1217 <cytops1217@gmail.com>
2019-10-10 23:51:01 +0900

    add room svg file to project

--- kono-front/src/components/RoomPanel.js
+++ kono-front/src/components/RoomPanel.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from '../styles/RoomPanel.module.scss';
+import { ReactComponent as RoomSVG } from '../res/icons/room.svg';
 
 export default () => {
     return (
         <div className={styles.RoomPanel}>
-
+            <RoomSVG />
         </div>
     );
 }
