5ededaddf189d35407c6544e99c0e1b54ba21278
inhibitor1217 <cytops1217@gmail.com>
2019-10-19 07:37:51 +0000

    fix webpack configuration for kono-api

--- kono-api/webpack.config.js
+++ kono-api/webpack.config.js
@@ -26,17 +26,18 @@ module.exports = {
         knex: 'commonjs knex'
     },
     plugins: [
-        new webpack.NormalModuleReplacementPlugin(/\.\.\/migrate/, '../util/noop.js'),
-	new webpack.NormalModuleReplacementPlugin(/\.\.\/seed/, '../util/noop.js'),
-	new webpack.IgnorePlugin(/mariasql/, /\/knex\//),
-	new webpack.IgnorePlugin(/mssql/, /\/knex\//),
-	new webpack.IgnorePlugin(/mysql/, /\/knex\//),
-	new webpack.IgnorePlugin(/mysql2/, /\/knex\//),
-	new webpack.IgnorePlugin(/oracle/, /\/knex\//),
-	new webpack.IgnorePlugin(/oracledb/, /\/knex\//),
-	new webpack.IgnorePlugin(/pg-query-stream/, /\/knex\//),
-	new webpack.IgnorePlugin(/sqlite3/, /\/knex\//),
-	new webpack.IgnorePlugin(/strong-oracle/, /\/knex\//),
-	new webpack.IgnorePlugin(/pg-native/, /\/pg\//)
+        new webpack.NormalModuleReplacementPlugin(/\.\.migrate/, '../util/noop.js'),
+        new webpack.NormalModuleReplacementPlugin(/\.\.seed/, '../util/noop.js'),
+        new webpack.IgnorePlugin(/mariasql/, /knex/),
+        new webpack.IgnorePlugin(/mysql2/, /knex/),
+        new webpack.IgnorePlugin(/mssql/, /knex/),
+        new webpack.IgnorePlugin(/oracle/, /knex/),
+        new webpack.IgnorePlugin(/oracledb/, /knex/),
+        new webpack.IgnorePlugin(/postgres/, /knex/),
+        new webpack.IgnorePlugin(/redshift/, /knex/),
+        new webpack.IgnorePlugin(/pg-query-stream/, /knex/),
+        new webpack.IgnorePlugin(/sqlite3/, /knex/),
+        new webpack.IgnorePlugin(/strong-oracle/, /knex/),
+        new webpack.IgnorePlugin(/pg-native/, /pg/)
     ]
 }
