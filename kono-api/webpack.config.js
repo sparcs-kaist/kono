2b692566a058a74f79e560c60c5d9aa78c8cb987
inhibitor1217 <cytops1217@gmail.com>
2019-10-19 08:21:43 +0000

    fix knex bundling in kono-api

--- kono-api/webpack.config.js
+++ kono-api/webpack.config.js
@@ -1,4 +1,5 @@
 const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'production',
@@ -21,7 +22,19 @@ module.exports = {
     optimization: {
         minimizer: [new TerserPlugin({ terserOptions: { mangle: false } })]
     },
-    externals: {
-        knex: 'commonjs knex'
-    }
+    plugins: [
+        new webpack.NormalModuleReplacementPlugin(/\.\.migrate/, '../util/noop.js'),
+        new webpack.NormalModuleReplacementPlugin(/\.\.seed/, '../util/noop.js'),
+        new webpack.IgnorePlugin(/mariasql/, /knex/),
+        new webpack.IgnorePlugin(/mysql2/, /knex/),
+        new webpack.IgnorePlugin(/mssql/, /knex/),
+        new webpack.IgnorePlugin(/oracle/, /knex/),
+        new webpack.IgnorePlugin(/oracledb/, /knex/),
+        new webpack.IgnorePlugin(/postgres/, /knex/),
+        new webpack.IgnorePlugin(/redshift/, /knex/),
+        new webpack.IgnorePlugin(/pg-query-stream/, /knex/),
+        new webpack.IgnorePlugin(/sqlite3/, /knex/),
+        new webpack.IgnorePlugin(/strong-oracle/, /knex/),
+        new webpack.IgnorePlugin(/pg-native/, /pg/)
+    ]
 }
