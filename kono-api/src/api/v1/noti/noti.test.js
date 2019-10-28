7f60a6f8bdad3b90cf4ce9a4761f905efb8d4a8f
inhibitor1217 <cytops1217@gmail.com>
2019-10-28 13:39:12 +0000

    write tests for PUT /api/v1/noti/:sid

--- kono-api/src/api/v1/noti/noti.test.js
+++ kono-api/src/api/v1/noti/noti.test.js
@@ -229,3 +229,170 @@ describe('Testing POST /api/v1/noti ...', () => {
     });
     
 });
+
+describe('Testing PUT /api/v1/noti/:sid', () => {
+
+    const testSingleUpdate = () => async () => {
+
+        const token = await generateToken({ sid: 0 });
+
+        /* Clean table. */
+        await db.instance('noti').del();
+        /* Put a noti of sid 1. */
+        await db.instance('noti').insert({ sid: 1, noti_kr: '테스트', noti_en: 'test' });
+
+        const res = await request(apiURL)
+            .put('/api/v1/noti/1')
+            .set('content-type', 'application/json')
+            .set('Cookie', `access_token=${token}`)
+            .send(JSON.stringify({ noti_kr: '테스트2', noti_en: 'test2' }));
+
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.keys([ 'sid', 'noti_kr', 'noti_en', 'created_time' ]);
+        const { sid, noti_kr, noti_en, created_time } = res.body;
+        expect(sid).to.equal(1);
+        expect(noti_kr).to.equal('테스트2');
+        expect(noti_en).to.equal('test2');
+        expect(created_time).to.satisfy(e => !isNaN(Date.parse(e)));
+
+        const [updatedNoti] = await db.instance.select('*').from('noti').where({ sid: 1 });
+        expect(updatedNoti.noti_kr).to.equal('테스트2');
+        expect(updatedNoti.noti_en).to.equal('test2');
+
+        /* Clean table. */
+        await db.instance('noti').del();
+
+    };
+
+    const testMultipleUpdate = () => async () => {
+
+        const token = await generateToken({ sid: 0 });
+
+        /* Clean table. */
+        await db.instance('noti').del();
+        /* Put a noti of sid 1~3. */
+        await db.instance('noti').insert({ sid: 1, noti_kr: '테스트1', noti_en: 'test1' });
+        await db.instance('noti').insert({ sid: 2, noti_kr: '테스트2', noti_en: 'test2' });
+        await db.instance('noti').insert({ sid: 3, noti_kr: '테스트3', noti_en: 'test3' });
+        
+        const res1 = await request(apiURL)
+            .put('/api/v1/noti/1')
+            .set('content-type', 'application/json')
+            .set('Cookie', `access_token=${token}`)
+            .send(JSON.stringify({ noti_kr: '흠...', noti_en: 'hmm...' }));
+
+        const res2 = await request(apiURL)
+            .put('/api/v1/noti/2')
+            .set('content-type', 'application/json')
+            .set('Cookie', `access_token=${token}`)
+            .send(JSON.stringify({ noti_kr: '잘됐을까?', noti_en: 'was it done well?' }));
+
+        const res3 = await request(apiURL)
+            .put('/api/v1/noti/3')
+            .set('content-type', 'application/json')
+            .set('Cookie', `access_token=${token}`)
+            .send(JSON.stringify({ noti_kr: '정말?', noti_en: 'please?' }));
+
+        expect(res1).to.have.status(200);
+        expect(res1.body).to.have.keys([ 'sid', 'noti_kr', 'noti_en', 'created_time' ]);
+        expect(res1.body.sid).to.equal(1);
+        expect(res1.body.noti_kr).to.equal('흠...');
+        expect(res1.body.noti_en).to.equal('hmm...');
+
+        expect(res2).to.have.status(200);
+        expect(res2.body).to.have.keys([ 'sid', 'noti_kr', 'noti_en', 'created_time' ]);
+        expect(res2.body.sid).to.equal(2);
+        expect(res2.body.noti_kr).to.equal('잘됐을까?');
+        expect(res2.body.noti_en).to.equal('was it done well?');
+
+        expect(res3).to.have.status(200);
+        expect(res3.body).to.have.keys([ 'sid', 'noti_kr', 'noti_en', 'created_time' ]);
+        expect(res3.body.sid).to.equal(3);
+        expect(res3.body.noti_kr).to.equal('정말?');
+        expect(res3.body.noti_en).to.equal('please?');
+
+        const notis = await db.instance.select('*').from('noti');
+        expect(notis[0].sid).to.equal(3);
+        expect(notis[0].noti_kr).to.equal('정말?');
+        expect(notis[0].noti_en).to.equal('please?');
+        expect(notis[1].sid).to.equal(2);
+        expect(notis[1].noti_kr).to.equal('잘됐을까?');
+        expect(notis[1].noti_en).to.equal('was it done well?');
+        expect(notis[2].sid).to.equal(1);
+        expect(notis[2].noti_kr).to.equal('흠...');
+        expect(notis[2].noti_en).to.equal('hmm...');
+
+        /* Clean table. */
+        await db.instance('noti').del();
+
+    };
+
+    const testInvalidQuery = (sid, body, invalidField) => async () => {
+
+        /* Clean table. */
+        await db.instance('noti').del();
+        /* Put a noti of sid 1~3. */
+        await db.instance('noti').insert({ sid: 1, noti_kr: '테스트1', noti_en: 'test1' });
+
+        const token = await generateToken({ sid: 0 });
+
+        const res = await request(apiURL)
+            .put(`/api/v1/noti/${sid}`)
+            .set('content-type', 'application/json')
+            .set('Cookie', `access_token=${token}`)
+            .send(JSON.stringify(body));
+
+        expect(res).to.have.status(400);
+        expect(res.body).to.have.key('msg');
+        expect(res.body.msg).to.equal(`invalid ${invalidField}`);
+
+        /* Clean table. */
+        await db.instance('noti').del();
+
+    };
+
+    const testForbidden = (sid, body) => async () => {
+
+        /* Clean table. */
+        await db.instance('noti').del();
+        /* Put a noti of sid 1~3. */
+        await db.instance('noti').insert({ sid: 1, noti_kr: '테스트1', noti_en: 'test1' });
+
+        const res = await request(apiURL)
+            .put(`/api/v1/noti/${sid}`)
+            .set('content-type', 'application/json')
+            .send(JSON.stringify(body));
+
+        expect(res).to.have.status(403);
+        expect(res.body).to.have.key('msg');
+        expect(res.body.msg).to.equal('login required');
+
+        /* Clean table. */
+        await db.instance('noti').del();
+        
+    }
+
+    it('Single update test.', testSingleUpdate());
+    it('Multiple update test.', testMultipleUpdate());
+    describe('Error handling tests.', () => {
+
+        it('Invalid field "noti_kr"', testInvalidQuery(1, {
+            noti_kr: 'ㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇㅅㅇ',
+            noti_en: 'test'
+        }, 'noti_kr'));
+        it('Invalid field "noti_en"', testInvalidQuery(1, {
+            noti_kr: '테스트',
+            noti_en: 'blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla'
+        }));
+        it('Not logged', testForbidden(1, {
+            noti_kr: '로그인 안 한 상태',
+            noti_en: 'I\'m not logged in!'
+        }));
+        it('Invalid parameter "sid"', testInvalidQuery(2, {
+            noti_kr: '테스트',
+            noti_en: 'test'
+        }));
+        
+    });
+
+});
