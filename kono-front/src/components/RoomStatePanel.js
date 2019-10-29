55fb8da2ff788066e5a78b024b014f57fd4e33a5
Suniel Nah <32695733+Sunnynah@users.noreply.github.com>
2019-10-30 03:25:34 +0900

    delete redundant code

--- kono-front/src/components/RoomStatePanel.js
+++ kono-front/src/components/RoomStatePanel.js
@@ -65,7 +65,6 @@ export default ({ rooms, highlight }) => {
                         position: 'absolute',
                         width: 545,
                         height:604,
-                        zIndex: room_idx,
                         pointerEvents: 'none',
                     }}
                     className={classnames([
