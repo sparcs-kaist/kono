17e84e924d6a2d9c0dc78edc1bbba17c1f44fcaf
inhibitor1217 <cytops1217@gmail.com>
2019-10-29 07:20:53 +0000

    implement GET /api/v1/noti

--- kono-api/src/api/v1/noti/noti.control.js
+++ kono-api/src/api/v1/noti/noti.control.js
@@ -2,8 +2,49 @@ import db from '../../../db';
 
 export const list = async (req, res) => {
 
-    res.status(200);
-    res.send('GET /api/v1/noti');
+    const { max_size } = req.query;
+
+    /* Query validity check. */
+    if (max_size === undefined) {
+        res.status(400);
+        res.send({ msg: 'invalid max_size' });
+        return;
+    }
+
+    const MAX_SIZE = parseInt(max_size);
+    if (isNaN(MAX_SIZE) || !Number.isSafeInteger(MAX_SIZE) || MAX_SIZE < 1 || MAX_SIZE > 64) {
+        res.status(400);
+        res.send({ msg: 'invalid max_size' });
+        return;
+    }
+
+    /* Fire database query. */
+    try {
+
+        const notis = await db.instance
+            .select('sid', 'noti_kr', 'noti_en', 'created_time')
+            .from('noti')
+            .where({ deleted: 0 })
+            .orderBy([ 
+                {
+                    column: 'created_time', 
+                    order: 'desc'
+                },
+                {
+                    column: 'sid', 
+                    order: 'desc'
+                }
+            ])
+            .limit(MAX_SIZE);
+
+        res.status(200);
+        res.send(notis);
+
+    } catch (e) {
+        console.log(e);
+        res.status(500);
+        res.send({ msg: 'server error' });
+    }
 
 };
 
