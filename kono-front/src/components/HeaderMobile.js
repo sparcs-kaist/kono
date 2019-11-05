6541ba81642c67d16a68ae3c22dd1eaf8bd6b641
inhibitor1217 <cytops1217@gmail.com>
2019-11-05 19:46:44 +0900

    add dropdown menu

--- kono-front/src/components/HeaderMobile.js
+++ kono-front/src/components/HeaderMobile.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../styles/HeaderMobile.module.scss';
+import classnames from '../lib/classnames';
+import { ReactComponent as Logo } from '../res/icons/logo.svg';
+import MaterialIcon from './MaterialIcon';
 
-export default () => {
+export default ({
+    text, login, 
+    onToggleTheme, onToggleLanguage, onLogout
+}) => {
+
+    const [dropdown, setDropdown] = useState(false);
+    const onToggleDropdown = () => { setDropdown(e => !e); }
+
     return (
-        <div>Mobile Header</div>
+        <div className={styles.HeaderMobile}>
+            <div className={styles.HeaderMobile__bar} />
+            <div className={styles.HeaderMobile__content}>
+                <div className={styles.HeaderMobile__logo}>
+                    <Link to="/">
+                        <Logo />
+                    </Link>
+                </div>
+                <div 
+                    className={classnames([
+                        styles.HeaderMobile__common_menu,
+                        dropdown && styles.HeaderMobile__menu_pressed
+                    ])}
+                    onClick={onToggleDropdown}
+                >
+                    <MaterialIcon md36>dehaze</MaterialIcon>
+                </div>
+            </div>
+        </div>
     )
 }
