18dd48882e7df3b693f8f0933b3ef88d5ba074da
inhibitor1217 <cytops1217@gmail.com>
2019-11-12 13:37:38 +0900

    fix LandingPage layout

--- kono-front/src/components/page/LandingPage.js
+++ kono-front/src/components/page/LandingPage.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import styles from 'styles/LandingPage.module.scss';
 import { NoticePanel, LostFoundPanel, RoomPanel } from 'components/landing';
+import classnames from 'lib/classnames';
 
 export default () => {
 
     return (
         <div className={styles.LandingPage}>
             <div className={styles.LandingPage__container}>
-                <RoomPanel />
+                <div className={classnames([
+                    styles.LandingPage__item,
+                    styles.LandingPage__item_single])}>
+                    <RoomPanel />
+                </div>
             </div>
             <div className={styles.LandingPage__container}>
                 <div className={styles.LandingPage__item}>
