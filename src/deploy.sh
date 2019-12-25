ec9ca26f6ac578e85c5392b40a1a22ea0d537e9e
inhibitor1217 <cytops1217@gmail.com>
2019-12-25 15:01:53 +0000

    integrate docker-compose configuration file

--- src/deploy.sh
+++ src/deploy.sh
@@ -7,11 +7,11 @@ git checkout production
 cd kono-api
 npm install
 npm run build
-cp -r dist /usr/src/api
+cp -r dist /usr/src/api-production
 cd ..
 
 cd kono-front
 npm install
 npm run build
-cp -r build /usr/src/app
+cp -r build /usr/src/app-production
 cd ..
