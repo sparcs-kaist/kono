829440344fcf115ca87913f9dbfb3cd2e0ff1afc
inhibitor1217 <cytops1217@gmail.com>
2019-12-29 17:53:52 +0900

    implement averaging latency

--- kono-judge/judge.py
+++ kono-judge/judge.py
@@ -1,4 +1,4 @@
-import websockets, asyncio, os, struct, json, collections, time, sys, pprint
+import os, websockets, asyncio, struct, json, collections, time
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,6 +12,7 @@ INTERVALS = {
     '24h'  : 24 * 60 * 60 * 1000
 }
 INTERVAL_KEYS = INTERVALS.keys()
+WEIGHT = 0.97
 
 metadata = { }
 datadump = { }
@@ -21,20 +22,6 @@ def millis():
 
 async def collector_handler(websocket, path):
     print('[kono-judge] Device Connected')
-    metadata[websocket] = {
-        'initialized': False,
-        'device_id'  : 0,
-        'offset'     : 0,
-        'latency'    : 0,
-        'buffer_size': {
-            '10sec': 0,
-            '1min' : 0,
-            '10min': 0,
-            '1h'   : 0,
-            '6h'   : 0,
-            '24h'  : 0
-        }
-    }
     try:
         async for message in websocket:
 
@@ -43,12 +30,24 @@ async def collector_handler(websocket, path):
             device_id = int.from_bytes(data_bin[1], 'little')
             data      = list(map(lambda x: struct.unpack('<f', x)[0], data_bin[2:]))
 
-            if not metadata[websocket]['initialized']:
-                metadata[websocket]['initialized'] = True
-                metadata[websocket]['device_id'] = device_id
-                metadata[websocket]['offset'] = millis() - timestamp
+            current_time = millis()
+
+            if device_id not in metadata:
+                metadata[device_id] = {
+                    'offset'     : current_time - timestamp,
+                    'last_packet': 0,
+                    'buffer_size': {
+                        '10sec': 0,
+                        '1min' : 0,
+                        '10min': 0,
+                        '1h'   : 0,
+                        '6h'   : 0,
+                        '24h'  : 0
+                    }
+                }
             
-            timestamp += metadata[websocket]['offset']
+            metadata[device_id]['last_packet'] = timestamp
+            metadata[device_id]['offset'] = WEIGHT * metadata[device_id]['offset'] + (1 - WEIGHT) * (current_time - timestamp)
 
             if device_id not in datadump:
                 datadump[device_id] = { }
@@ -56,31 +55,20 @@ async def collector_handler(websocket, path):
                     datadump[device_id][key] = collections.deque()
 
             for key in INTERVAL_KEYS:
-                datadump[device_id][key].append((timestamp, data))
-
-            t = millis()
-
-            metadata[websocket]['latency'] = t - timestamp
+                datadump[device_id][key].append((timestamp + metadata[device_id]['offset'], data))
 
             for key in INTERVAL_KEYS:
-                while datadump[device_id][key][0][0] + INTERVALS[key] < t:
+                while datadump[device_id][key][0][0] + INTERVALS[key] < current_time:
                     datadump[device_id][key].popleft()
 
             for key in INTERVAL_KEYS:
-                metadata[websocket]['buffer_size'][key] = len(datadump[device_id][key])
-
-            os.system('clear')
-            sys.stdout.write('server time: %d\n' % t)
-            sys.stdout.write('packet time: %d\n' % timestamp)
-            sys.stdout.write('%s'   % str(pprint.pformat(metadata)))
-            sys.stdout.flush()
+                metadata[device_id]['buffer_size'][key] = len(datadump[device_id][key])
             
             # reply
             await websocket.send(data_bin[0])
 
     finally:
         print('[kono-judge] Device Disconnected')
-        del metadata[websocket]
 
 def main():
     start_server = websockets.serve(collector_handler, host='0.0.0.0', port=PORT)
