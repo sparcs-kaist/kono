cf54a7fe8b44a6c6ef9fa0630459fff42818648e
inhibitor1217 <cytops1217@gmail.com>
2020-01-02 13:45:24 +0900

    write comments

--- kono-judge/src/client.py
+++ kono-judge/src/client.py
@@ -1,3 +1,8 @@
+# client.py
+#   Sample client to test kono-judge
+# 
+# bash> python3 client.py
+
 import asyncio
 import websockets
 from websockets.exceptions import ConnectionClosedOK, ConnectionClosedError
@@ -8,30 +13,42 @@ load_dotenv()
 WEBSOCKET_PORT = os.getenv('WEBSOCKET_PORT')
 
 async def connect():
+    # Connection coroutine
     uri = f"ws://localhost:{WEBSOCKET_PORT}"
     return await websockets.connect(uri)
 
 async def client(websocket):
+    # Message handling coroutine
     try:
+        # Send sampled data
+        # - bits[0:3] are timestamp
+        # - bits[4:7] are device id
+        # - Little endian representation
         await websocket.send(b'\x00\x00\x00\x00\x4C\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+        # Print received messages from now on
         async for message in websocket:
             print(f'[Client] Received: {message}')
     except ConnectionClosedOK:
+        # Received closing frame from the server
         print('[Client] Connection refused from server')
     except ConnectionClosedError:
+        # Did not receive closing frame from the server
         print('[Client] Connection closed without close frame') 
     finally:
         print('[Client] Server closed connection')
 
 async def close(websocket):
+    # Cleanup
     await websocket.close()
 
 def main():
     loop = asyncio.get_event_loop()
+    # Retrieve connection context and connect to server
     websocket = loop.run_until_complete(connect())
     try:
         loop.run_until_complete(client(websocket))
     except KeyboardInterrupt:
+        # Cleanup
         loop.run_until_complete(close(websocket))
         print('[Client] Closed connection')
 
--- kono-judge/src/judge.py
+++ kono-judge/src/judge.py
@@ -1,3 +1,11 @@
+# judge.py
+#   Main file for kono-judge
+#   Depends on:
+#       - data.py   (data structure for storing arduino data packets)
+#       - router.py (router of the HTTP server)
+# 
+# bash> python3 judge.py
+
 import os, websockets, asyncio, struct, collections, time, json
 from websockets.exceptions import ConnectionClosedError
 from dotenv import load_dotenv
@@ -5,13 +13,16 @@ from aiohttp.web import Application, AppRunner, TCPSite
 from router import Router
 from data   import Datadump
 
+# Load environment variables (.env on local development, or set by docker-compose)
 load_dotenv()
-WEBSOCKET_PORT = os.getenv('WEBSOCKET_PORT')
-HTTP_PORT      = os.getenv('HTTP_PORT')
+WEBSOCKET_PORT = os.getenv('WEBSOCKET_PORT')    # Port used for websocket server
+HTTP_PORT      = os.getenv('HTTP_PORT')         # Port used for RESTful HTTP server
 
+# Create data structure (data.py)
 datadump = Datadump()
 
 async def http_server():
+    # Create context for HTTP server
     app = Application()
     router = Router(datadump)
     router.register(app.router)
@@ -21,19 +32,27 @@ async def http_server():
     return runner, site
 
 async def close_http_server(runner):
+    # Cleanup routine for HTTP server
     print('[kono-judge] Closing HTTP server...')
     await runner.cleanup()
 
-WEIGHT = 0.97
-MAX_CONNECTIONS     = 9
-MAX_STATUS_CLIENTS  = 2
+# Global constants for websocket server
+WEIGHT = 0.97               # Weight of moving average algorithm, for calculating connection latency
+MAX_CONNECTIONS     = 9     # Max number of total websocket connections available
+MAX_STATUS_CLIENTS  = 2     # Max number of kono-judge-status clients available
 
+# Confidentials (refer to docs.kono.sparcs.org)
 confidentials = json.loads(open('confidentials.json').read())
 
-metadata = { }
-connections = []
-arduino_clients = { }
-status_clients  = []
+# Global data structure
+metadata        = {}    # Storage of arduino websocket metadata 
+                        #   device_id -> { 
+                        #       offset,         // latency (difference between local time and server time)
+                        #       last_packet     // Last received packet timestamp (in local time)
+                        #   }
+connections     = []    # Storage of all websocket context
+arduino_clients = {}    # Storage of arduino websocket context (websocket -> device_id)
+status_clients  = []    # Storage of kono-judge-status clients
 
 def get_data(device_id):
     if device_id in datadump:
@@ -45,6 +64,7 @@ def millis():
     return int(round(time.time() * 1000))
 
 async def collector_handler(websocket, path):
+    # MAX_CONNECTION check
     if len(connections) >= MAX_CONNECTIONS:
         print('[kono-judge] Connection refused by max connection limit')
         await websocket.send('[kono-judge] Max connection limit exceeded')
@@ -53,9 +73,15 @@ async def collector_handler(websocket, path):
     else:
         print('[kono-judge] Device Connected')
         connections.append(websocket)
+    
+    # Main message handling routine
     try:
         async for message in websocket:
 
+            # Parse received packet
+            #   - message[0:3] = timestamp (int,   little endian)
+            #   - message[4:7] = device_id (int,   little endian, -1 if kono-judge-status)
+            #   - message[8:]  = data[0:5] (float, little endian)
             data_bin  = list(map(lambda i: message[4*i : 4*(i+1)], range(8)))
             timestamp = int.from_bytes(data_bin[0], 'little')
             device_id = int.from_bytes(data_bin[1], 'little')
@@ -64,6 +90,8 @@ async def collector_handler(websocket, path):
             current_time = millis()
 
             if device_id == 0xFFFFFFFF:
+                # Client is kono-judge-status
+                # MAX_STATUS_CLIENTS check
                 if len(status_clients) >= MAX_STATUS_CLIENTS:
                     await websocket.send('[kono-judge] Connection refused by max status client limit')
                 else:
@@ -71,7 +99,10 @@ async def collector_handler(websocket, path):
                     await websocket.send('[kono-judge] Connected')
                 continue
             else:
+                # Client is kono-arduino
+                # device_id check
                 if device_id in confidentials['allowed_device_ids']:
+                    # duplicate connection check
                     if device_id in arduino_clients.values():
                         await websocket.send(f'[kono-judge] The device {device_id} is already connected. Are you sure you are the right device?')
                         print(f'[kono-judge] Dupulicate connection attempt from device {device_id}')
@@ -84,30 +115,37 @@ async def collector_handler(websocket, path):
                     await websocket.close()
                     continue
 
+            # Initialize metadata data structure
             if device_id not in metadata:
                 metadata[device_id] = {
                     'offset'     : current_time - timestamp,
                     'last_packet': 0
                 }
 
+            # Update metadata
+            # Moving average for calculating offset
             metadata[device_id]['last_packet'] = timestamp
             metadata[device_id]['offset'] = WEIGHT * metadata[device_id]['offset'] + (1 - WEIGHT) * (current_time - timestamp)
 
+            # Insert new data from the packet
             new_data = {
                 'timestamp': round(timestamp + metadata[device_id]['offset']),
                 'device_id': device_id,
                 'data': data
             }
-
             datadump.insert(device_id, new_data, current_time)
 
+            # Forward new data to kono-judge-status clients
             if len(status_clients) > 0:
                 await asyncio.wait([client.send(json.dumps(new_data)) for client in status_clients])
+            # Acknowledge received packet back to kono-arduino
             await websocket.send(data_bin[0])
 
+    # Error handling routine
     except ConnectionClosedError:
         print('[kono-judge] Connection closed without close frame')
 
+    # Cleanup
     finally:
         indicator = f'device {arduino_clients[websocket]}' if websocket in arduino_clients else 'kono-status'
         print(f'[kono-judge] Disconnected: {indicator}')
@@ -119,6 +157,7 @@ async def collector_handler(websocket, path):
             status_clients.remove(websocket)
 
 def websocket_server():
+    # Returns websocket server context
     return websockets.serve(
         collector_handler, 
         host='0.0.0.0', port=WEBSOCKET_PORT,
@@ -132,14 +171,22 @@ async def close_websocket_server():
 
 def main():
     loop = asyncio.get_event_loop()
+
+    # Retrieve websocket server and HTTP server contexts
     websocket_runner       = websocket_server()
     http_runner, http_site = loop.run_until_complete(http_server())
+
     try:
+        # Run two servers in separate coroutines
         asyncio.gather(websocket_runner, http_site.start())
         loop.run_forever()
+
     except KeyboardInterrupt:
+        # Handle Ctrl+C (SIGINT) interrupt
+        # Execute cleanup coroutines
         loop.run_until_complete(close_websocket_server())
         loop.run_until_complete(close_http_server(http_runner))
+        
         print('[kono-judge] Exiting...')
 
 if __name__ == '__main__':
