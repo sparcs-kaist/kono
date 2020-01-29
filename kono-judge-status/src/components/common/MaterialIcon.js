89d876cdecf3ab2200d692c36ec6c638cbaca945
inhibitor1217 <inhibitor@kaist.ac.kr>
2020-01-29 23:41:56 +0900

    create view displaying websocket state

--- kono-judge-status/src/components/common/MaterialIcon.js
+++ kono-judge-status/src/components/common/MaterialIcon.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from 'styles/components/MaterialIcon.module.scss';
 
-export default ({ children, style }) => {
+export default ({ children, fontSize, style }) => {
     return (
-        <div className={styles.MaterialIcon} style={style}>
+        <div className={styles.MaterialIcon} style={{
+            fontSize, ...style
+        }}>
             { children }
         </div>
     )
--- kono-judge-status/src/components/websocket/WebsocketController.js
+++ kono-judge-status/src/components/websocket/WebsocketController.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import styles from 'styles/components/WebsocketController.module.scss';
 import { MaterialIcon, Spinner } from 'components/common';
 import { WebsocketContext } from 'components/provider/WebsocketProvider';
+import * as WebsocketConstants from 'lib/websocket/constants';
 
 export default () => {
 
@@ -9,8 +10,34 @@ export default () => {
 
     return (
         <div className={styles.WebsocketController}>
-            <MaterialIcon>wifi</MaterialIcon>
-            <Spinner />
+            <div className={styles.state_wrapper}>
+                {
+                    ((state) => {
+                        switch (state) {
+                        case WebsocketConstants.STATE_CLOSED:
+                            return (
+                                <MaterialIcon fontSize={18}>wifi_off</MaterialIcon>
+                            );
+                        case WebsocketConstants.STATE_OPEN:
+                            return (
+                                <MaterialIcon fontSize={18}>wifi</MaterialIcon>
+                            );
+                        case WebsocketConstants.STATE_CONNECTING:
+                        case WebsocketConstants.STATE_CLOSING:
+                            return (
+                                <Spinner size={18} />
+                            );
+                        default:
+                            return null;
+                        }
+                    })(state)
+                }
+                <span>{ state }</span>
+            </div>
+            <div className={styles.button_wrapper}>
+                <div onClick={open}>CONNECT</div>
+                <div onClick={close}>DISCONENCT</div>
+            </div>
         </div>
     )
 
