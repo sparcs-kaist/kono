6f268e987b9548e942465db9d1f88bd0dc485dda
inhibitor1217 <inhibitor@kaist.ac.kr>
2020-02-01 16:14:27 +0900

    add scss LandingPage layout

--- kono-judge-status/src/components/page/LandingPage.js
+++ kono-judge-status/src/components/page/LandingPage.js
@@ -1,31 +1,36 @@
 import React, { useState, useContext } from 'react';
+import styles from 'styles/components/LandingPage.module.scss';
 import { WebsocketController } from 'components/websocket';
 import { FilterSelector, DeviceSelector } from 'components/data';
 import { TIME_FILTERS, DataContext } from 'components/provider/DataProvider';
+import classnames from 'lib/classnames';
 
 export default () => {
 
     const { deviceIDs } = useContext(DataContext);
-    const [selectedDevice, setSelectedDevice] = useState(null);
+    const [selectedDeviceID, setSelectedDeviceID] = useState(null);
     const [selectedFilter, setSelectedFilter] = useState(TIME_FILTERS[0]);
 
     return (
-        <div>
-            <div>
+        <div className={styles.LandingPage}>
+            <div className={styles.content}>
                 Data
             </div>
-            <div>
-                <div>
+            <div className={styles.sidebar}>
+                <div className={styles.sidebar_item}>
                     <WebsocketController />
                 </div>
-                <div>
+                <div className={classnames([
+                    styles.sidebar_item,
+                    styles.sidebar_item_stretch
+                ])}>
                     <DeviceSelector 
                         deviceIDs={deviceIDs}
-                        selectedDevice={selectedDevice}
-                        setSelectedDevice={setSelectedDevice}
+                        selectedDeviceID={selectedDeviceID}
+                        setSelectedDeviceID={setSelectedDeviceID}
                     />
                 </div>
-                <div>
+                <div className={styles.sidebar_item}>
                     <FilterSelector 
                         filters={TIME_FILTERS}
                         selectedFilter={selectedFilter}
