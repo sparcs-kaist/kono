ab0928d38eee5d8e20de69a97de81166d6e8d677
inhibitor1217 <inhibitor@kaist.ac.kr>
2020-02-03 23:13:03 +0900

    change some color

--- kono-judge-status/src/components/data/HistoryEditor.js
+++ kono-judge-status/src/components/data/HistoryEditor.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import styles from 'styles/components/HistoryEditor.module.scss';
+import classnames from 'lib/classnames';
 
 export default ({ deviceIDs }) => {
 
     return (
         <div className={styles.HistoryEditor}>
             <div className={styles.content_wrapper}>
-                
+                {
+                    deviceIDs.map(deviceID => {
+                        return (
+                            <div
+                                className={styles.item}
+                                key={`history-${deviceID}`}>
+                                <span>{ deviceID }</span>
+                                <div className={classnames([
+                                    styles.item_button,
+                                    styles.button_occupied
+                                ])}>
+                                    Occupied
+                                </div>
+                                <div className={classnames([
+                                    styles.item_button,
+                                    styles.button_vacant
+                                ])}>
+                                    Vacant
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
