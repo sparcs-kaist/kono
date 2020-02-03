95ec04d7208cb0397a53dcfefbd2cd488c03c205
inhibitor1217 <inhibitor@kaist.ac.kr>
2020-02-03 23:05:31 +0900

    add history editor panel

---
+++ kono-judge-status/src/components/data/HistoryEditor.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from 'styles/components/HistoryEditor.module.scss';
+
+export default ({ deviceIDs }) => {
+
+    return (
+        <div className={styles.HistoryEditor}>
+            <div className={styles.content_wrapper}>
+                
+            </div>
+        </div>
+    )
+
+}
