a34c613ba34ae98614a927f9045d94fb0101e53f
inhibitor1217 <inhibitor@kaist.ac.kr>
2020-02-06 23:33:48 +0900

    animate state editor panel

--- kono-judge-status/src/components/data/HistoryEditor.js
+++ kono-judge-status/src/components/data/HistoryEditor.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from 'styles/components/HistoryEditor.module.scss';
 import classnames from 'lib/classnames';
+import { MaterialIcon } from 'components/common';
 
 export default ({ deviceIDs }) => {
+
+    const [selectedDropdown, setSelectedDropdown] = useState(null);
 
     return (
         <div className={styles.HistoryEditor}>
@@ -10,11 +13,23 @@ export default ({ deviceIDs }) => {
                 <span className={styles.title}>State Editor</span>
                 {
                     deviceIDs.map(deviceID => {
+                        const hide = (selectedDropdown !== null) && (selectedDropdown !== deviceID);
+                        const selected = (selectedDropdown === deviceID);
+                        const onToggleHide = () => {
+                            if (selected)
+                                setSelectedDropdown(null);
+                            else
+                                setSelectedDropdown(deviceID);
+                        }
                         return (
                             <div
-                                className={styles.item}
+                                className={classnames([
+                                    styles.item,
+                                    selected && styles.item_selected,
+                                    hide && styles.item_hide
+                                ])}
                                 key={`history-${deviceID}`}>
-                                <span>{ deviceID }:</span>
+                                <span>{ deviceID }</span>
                                 <div className={classnames([
                                     styles.item_button
                                 ])}>
@@ -24,6 +39,14 @@ export default ({ deviceIDs }) => {
                                     styles.item_button
                                 ])}>
                                     Vacant
+                                </div>
+                                <div className={styles.dropdown}
+                                    onClick={onToggleHide}>
+                                    <MaterialIcon fontSize={16}>
+                                        {
+                                            selected ? 'clear' : 'keyboard_arrow_down'
+                                        }
+                                    </MaterialIcon>
                                 </div>
                             </div>
                         )
