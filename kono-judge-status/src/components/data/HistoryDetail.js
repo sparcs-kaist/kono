a6fd768d87616cb0c0aed097093c296f25681428
inhibitor1217 <inhibitor@kaist.ac.kr>
2020-02-07 13:59:55 +0900

    add css grid view

--- kono-judge-status/src/components/data/HistoryDetail.js
+++ kono-judge-status/src/components/data/HistoryDetail.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import styles from 'styles/components/HistoryDetail.module.scss';
 import { HistoryContext } from 'components/provider/HistoryProvider';
+import classnames from 'lib/classnames';
 
 function filterHistory(history, deviceID) {
     const filtered = [];
@@ -18,11 +19,41 @@ export default ({ deviceID }) => {
         return null;
 
     const { history } = useContext(HistoryContext);
+    const filteredHistory = filterHistory(history, deviceID);
 
     return (
         <div className={styles.HistoryDetail}>
-            <div className={styles.content}>  
-                { JSON.stringify(filterHistory(history, deviceID)) }
+            <div className={styles.content}> 
+                <div className={classnames([
+                    styles.grid_item,
+                    styles.grid_item_time
+                ])}>Time</div>
+                <div className={classnames([
+                    styles.grid_item,
+                    styles.grid_item_change
+                ])}>Change</div> 
+                {
+                    filteredHistory.map(({ timestamp, change }) => {
+                        return (
+                            <>
+                                <div className={classnames([
+                                    styles.grid_item,
+                                    styles.grid_item_time
+                                ])}
+                                    key={`grid-${timestamp}-time`}>
+                                    { timestamp }
+                                </div>
+                                <div className={classnames([
+                                    styles.grid_item,
+                                    styles.grid_item_change
+                                ])}
+                                    key={`grid-${timestamp}-change`}>
+                                    { change ? 'true' : 'false' }
+                                </div>
+                            </>
+                        )
+                    })
+                }
             </div>
         </div>
     )
