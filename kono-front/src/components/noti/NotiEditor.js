92c70be1d35f60ee8f571d69ae389608cfd4c27b
inhibitor1217 <inhibitor@kaist.ac.kr>
2020-02-22 23:32:49 +0900

    implement NotiEditor on active state

--- kono-front/src/components/noti/NotiEditor.js
+++ kono-front/src/components/noti/NotiEditor.js
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from 'styles/NotiEditor.module.scss';
 import { MaterialIcon } from 'components/common';
 import classnames from 'lib/classnames';
 
-export default ({ active }) => {
+const TRANSITION_ANIMATION_DELAY_MS = 300;
+
+export default ({ initialActive = false }) => {
+
+    const [active, setActive] = useState(initialActive);
+    const [activeStyle, setActiveStyle] = useState(initialActive);
+
+    const onClickIcon = () => {
+        if (!active) {
+            setActive(true);
+            setActiveStyle(true);
+        }
+    }
+    const onClickExit = () => {
+        setActiveStyle(false);
+        setTimeout(() => setActive(false), TRANSITION_ANIMATION_DELAY_MS)
+    };
 
     return (
         <div className={styles.NotiEditor}>
-            {
-                !active && (
-                    <div className={classnames([
-                        styles.non_active,
-                        styles.container
-                    ])}>
-                        <div className={classnames([
-                            styles.icon,
-                            styles.clickable
-                        ])}>
-                            <MaterialIcon>add</MaterialIcon>
+            <div className={classnames([
+                styles.non_active,
+                styles.container,
+                activeStyle && styles.active
+            ])}>
+                <div className={classnames([
+                    styles.icon,
+                    styles.clickable
+                ])} onClick={onClickIcon}>
+                    {
+                        <MaterialIcon>
+                            {
+                                active
+                                ? 'check_circle_outline'
+                                : 'add_circle_outline'
+                            }
+                        </MaterialIcon>
+                    }
+                </div>
+                {
+                    active && (
+                        <div className={classnames([styles.item, styles.edit_wrapper])}>
+                            <p>적을 내용</p>
+                            <p>Some Stuff</p>
                         </div>
-                    </div>
-                )
-            }
+                    )
+                }
+                {
+                    active && (
+                        <div className={classnames([styles.item, styles.clickable])}
+                            onClick={onClickExit}>
+                            <MaterialIcon>clear</MaterialIcon>
+                        </div>
+                    )
+                }
+            </div>
         </div>
     );
 
